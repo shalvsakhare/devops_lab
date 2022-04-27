269a9f6afc63eea0d1b9e4153c7a8bfb9fbd40c9
shalvsakhare <66238194+shalvsakhare@users.noreply.github.com>
2022-04-27 10:10:38 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,18 @@
+
+
+class add 
+	{
+	public static void main(String[]args)
+		{
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+
+			System.out.println("Addition of two numbers " +a+ " " +b+ "is:" +(a+b));
+			System.out.println("Subtraction of two numbers"  +a + " " +b + "is:" +(a-b));
+			System.out.println("Multiplication of two numbers"  +a + " " +b + "is:" +(a*b));	
+			System.out.println("Division of two numbers"  +a + " " +b + "is:" +(a/b));	
+
+				}
+
+
+	}

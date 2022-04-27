a52662e98ad4e030a325640078f9127855b85a58
shalvsakhare <66238194+shalvsakhare@users.noreply.github.com>
2022-04-27 12:08:47 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,17 @@
+
+
+class program 
+	{
+	public static void main(String[]args)
+		{
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+                        System.out.println(" "+a);
+                        System.out.println(" "+b);
+			System.out.println("Addition of two numbers is:" +(a+b));
+				
+
+				}
+
+
+	}
